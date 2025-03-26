b8edd5c134993822c211b632216e5bf0f6f9937d
이 형준 <hwak021@lutes.co.kr>
2025-03-26 13:32:41 +0900

    fix: studyCafe 리팩토링_1

--- src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
@@ -2,9 +2,9 @@ package cleancode.studycafe.tobe;
 
 public class StudyCafeApplication {
 
-    public static void main(String[] args) {
-        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
-        studyCafePassMachine.run();
-    }
+  public static void main(String[] args) {
+    StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+    studyCafePassMachine.run();
+  }
 
 }
