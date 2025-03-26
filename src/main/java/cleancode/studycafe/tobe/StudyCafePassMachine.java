c3824fe0ef18136d2e54371e94787efebed2f21f
이 형준 <hwak021@lutes.co.kr>
2025-03-26 17:04:50 +0900

    fix: studyCafe 리팩토링_6 - FileHandler를 Provider 개념으로 리팩토링

--- src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
@@ -7,6 +7,8 @@ import cleancode.studycafe.tobe.model.order.StudyCafePassOrder;
 import cleancode.studycafe.tobe.model.pass.*;
 import cleancode.studycafe.tobe.model.pass.loccker.StudyCafeLockerPass;
 import cleancode.studycafe.tobe.model.pass.loccker.StudyCafeLockerPasses;
+import cleancode.studycafe.tobe.provider.LockerPassProvider;
+import cleancode.studycafe.tobe.provider.SeatPassProvider;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,7 +16,13 @@ import java.util.Optional;
 public class StudyCafePassMachine {
 
   private final StudyCafeIOHandler ioHandler = new StudyCafeIOHandler();
-  private final StudyCafeFileHandler studyCafeFileHandler = new StudyCafeFileHandler();
+  private final SeatPassProvider seatPassProvider;
+  private final LockerPassProvider lockerPassProvider;
+
+  public StudyCafePassMachine(SeatPassProvider seatPassProvider, LockerPassProvider lockerPassProvider) {
+    this.seatPassProvider = seatPassProvider;
+    this.lockerPassProvider = lockerPassProvider;
+  }
 
   public void run() {
     try {
@@ -44,7 +52,7 @@ public class StudyCafePassMachine {
   }
 
   private List<StudyCafeSeatPass> findPassCandidatesBy(StudyCafePassType studyCafePassType) {
-    StudyCafeSeatPasses allPasses = studyCafeFileHandler.readStudyCafePasses();
+    StudyCafeSeatPasses allPasses = seatPassProvider.getSeatPasses();
 
     return allPasses.findPassBy(studyCafePassType);
   }
@@ -71,7 +79,7 @@ public class StudyCafePassMachine {
   }
 
   private Optional<StudyCafeLockerPass> findLockerPassCandidateBy(StudyCafeSeatPass pass) {
-    StudyCafeLockerPasses allLockerPasses = studyCafeFileHandler.readLockerPasses();
+    StudyCafeLockerPasses allLockerPasses = lockerPassProvider.getLockerPasses();
 
     return allLockerPasses.findLockerPassBy(pass);
   }
---
+++ src/main/java/cleancode/studycafe/tobe/provider/LockerPassProvider.java
@@ -0,0 +1,8 @@
+package cleancode.studycafe.tobe.provider;
+
+import cleancode.studycafe.tobe.model.pass.loccker.StudyCafeLockerPasses;
+
+public interface LockerPassProvider {
+
+  StudyCafeLockerPasses getLockerPasses();
+}
